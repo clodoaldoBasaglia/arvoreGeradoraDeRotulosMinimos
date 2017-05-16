d6831a32557bf97250bb8fc1e2d5e9adb5e03b68
luisfernandolfc <luisfernnando@gmail.com>
2017-05-16 14:04:50 -0300

    Armazenar grafo no array

--- src/arvoregeradoraderotulosminimos/LerArquivo.java
+++ src/arvoregeradoraderotulosminimos/LerArquivo.java
@@ -15,39 +15,119 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.lang.model.type.NullType;
+
 /**
  *
- * @author clodoaldo
+ * @author luisfernando
  */
 public class LerArquivo {
 
-    public File LerArquivo(String caminho) {
-        File arq = new File(caminho);
-        if (arq.isFile()) {
-            return arq;
+//    public File LerArquivo(String caminho) {
+//        File arq = new File(caminho);
+//        if (arq.isFile()) {
+//            return arq;
+//        }
+//        return null;
+//    }
+//
+//    String lerArquivo(String caminho) {
+//        try {
+//            List<String> linhas = new ArrayList<String>();
+//            File arq = new File(caminho);
+//            BufferedReader bfr = null;
+//            bfr = new BufferedReader(new FileReader(arq));
+//            try {
+//                while (bfr.readLine() != null) {
+//                    String teste = bfr.readLine();
+//                    System.out.println(teste);
+//                }
+//
+//            } catch (IOException ex) {
+//                Logger.getLogger(LerArquivo.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(LerArquivo.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return " ";
+//    }
+	public static ArrayList <Integer[][]> leArquivo(String caminho) throws IOException {
+        File arquivo = new File(caminho);
+        FileReader fr = new FileReader(arquivo);
+        BufferedReader br = new BufferedReader(fr);
+
+        if (!arquivo.exists()) {
+            throw new IOException("Arquivo nao encontrado");
         }
-        return null;
-    }
 
-    String lerArquivo(String caminho) {
-        try {
-            List<String> linhas = new ArrayList<String>();
-            File arq = new File(caminho);
-            BufferedReader bfr = null;
-            bfr = new BufferedReader(new FileReader(arq));
-            try {
-                while (bfr.readLine() != null) {
-                    String teste = bfr.readLine();
-                    System.out.println(teste);
-                }
+        //Linha 1 do arquivo
+        String linha = br.readLine();
+        String[] s = linha.split(" ");
 
-            } catch (IOException ex) {
-                Logger.getLogger(LerArquivo.class.getName()).log(Level.SEVERE, null, ex);
+        int vertices = Integer.parseInt(s[0]);
+        int rotulos = Integer.parseInt(s[1]);
+
+        int indice = vertices;
+        System.out.println("vertices: " + indice);
+        
+        ArrayList <Integer[][]> Grafos = new ArrayList<>();
+        Integer[][] matrizAdjacencia = new Integer[vertices][vertices];
+               
+        int i = 0;
+        int aux = 0;
+        int pos = 0;
+
+        while (((linha = br.readLine()) != null)) {
+            if (i % vertices == 0) {
+            	if (aux != 0) {
+            		Grafos.add(matrizAdjacencia);
+            		matrizAdjacencia = new Integer[vertices][vertices];
+            	}
+            	i = 0;
             }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(LerArquivo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return " ";
-    }
+            s = linha.split(" ");
+            
+            for (int j = 0; j < s.length; j++) {
 
+                if (s[j].equals("")) {
+                	matrizAdjacencia[indice - i - 1][j] = vertices;
+                }
+                else
+                	matrizAdjacencia[indice - i - 1][j] = Integer.parseInt(s[j]);
+            }
+            aux++;
+            i++;
+        }
+
+        br.close();
+        fr.close();
+        
+        for (Integer[][] grafo : Grafos){
+        	printMatriz(grafo, vertices);
+        }
+        
+        return Grafos;
+    }
+   
+//  public static Integer[][] inicializaMatriz(int tamanho) {
+//        Integer matriz[][] = new Integer[tamanho][tamanho];
+//        for (int i = 0; i < tamanho; i++) {
+//            for (int j = 0; j < tamanho; j++) {
+//                matriz[i][j] = -1;
+//            }
+//        }
+//        return matriz;
+//    }
+  
+  public static void printMatriz(Integer[][] matriz, int n) {
+        System.out.println("print matriz:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
+
+
