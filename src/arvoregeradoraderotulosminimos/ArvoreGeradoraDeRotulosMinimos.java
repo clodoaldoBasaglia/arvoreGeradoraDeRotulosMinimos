427803ecdb227ec7f50b34cd0267798bafe09023
Clodoaldo Basaglia <clodoaldofonseca92@gmail.com>
2017-05-16 00:58:40 -0300

    leitura de todos os 72 arquivos

--- src/arvoregeradoraderotulosminimos/ArvoreGeradoraDeRotulosMinimos.java
+++ src/arvoregeradoraderotulosminimos/ArvoreGeradoraDeRotulosMinimos.java
@@ -21,21 +21,27 @@ public class ArvoreGeradoraDeRotulosMinimos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        long inicio = System.currentTimeMillis() % 1000;
         LerArquivo la = new LerArquivo();
         String caminho = "/home/clodoaldo/Documentos/APS.Teoria.dos.Grafos.2017.1/instancias/";
         SondaDeArquivos sdd = new SondaDeArquivos();
+        File arq = null;
         Map<String, ArrayList<String>> sondaDeArquivos = sdd.sondaDeArquivos(caminho);
         Iterator<Map.Entry<String, ArrayList<String>>> iterator = sondaDeArquivos.entrySet().iterator();
+        ArrayList<File> arrayDeArquivo = new ArrayList<>();
         while (iterator.hasNext()) {
             String chave = iterator.next().getKey();
             ArrayList<String> get = sondaDeArquivos.get(chave);
             for (String string : get) {
-//                System.out.println(caminho+chave+"/"+string);
-                File arquivo = la.LerArquivo(caminho);
-                System.out.println(arquivo.getTotalSpace());
+//                System.out.println(caminho + chave + "/" + string);
+                arq = la.LerArquivo(caminho + chave + "/" + string);
+                arrayDeArquivo.add(arq);
             }
         }
-
+        long fim = System.currentTimeMillis() % 1000;
+        for (File file : arrayDeArquivo) {
+            
+        }
     }
 
 }
