f1ff2dae59dbb5cadbfa59026c024b4de53c0e11
ClodoaldoBasaglia <clodoaldofonseca92@gmail.com>
2017-05-22 15:25:52 -0300

    Nova SondaDeArquivosWindows para rodar em sistemas Windows, já que meu notebook morreu. Todas as estruturas estão prontas para o inicio do processo, é necessario discutir qual método sera utilizado para tal.

--- src/arvoregeradoraderotulosminimos/ArvoreGeradoraDeRotulosMinimos.java
+++ src/arvoregeradoraderotulosminimos/ArvoreGeradoraDeRotulosMinimos.java
@@ -27,6 +27,7 @@ public class ArvoreGeradoraDeRotulosMinimos {
         // TODO code application logic here
         LerArquivo la = new LerArquivo();
         String caminho = "/home/clodoaldo/Documentos/APS.Teoria.dos.Grafos.2017.1/instancias/";
+//        String caminho = "C:\\Users\\Suporte\\Documents\\APS.Teoria.dos.Grafos.2017.1\\instancias";
         SondaDeArquivos sdd = new SondaDeArquivos();
         Map<String, ArrayList<String>> sondaDeArquivos = sdd.sondaDeArquivos(caminho);
         Iterator<Map.Entry<String, ArrayList<String>>> iterator = sondaDeArquivos.entrySet().iterator();
