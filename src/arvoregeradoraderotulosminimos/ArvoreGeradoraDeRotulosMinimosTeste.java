6344b4964a51ee281a4faa1fd4db4f965d4f4e0a
luisfernandolfc <luisfernnando@gmail.com>
2017-06-13 19:31:37 -0300

    Configurando Classe de Teste
    Estruturando como deve ser feito para que o arquivo Json fique da forma correta

--- src/arvoregeradoraderotulosminimos/ArvoreGeradoraDeRotulosMinimosTeste.java
+++ src/arvoregeradoraderotulosminimos/ArvoreGeradoraDeRotulosMinimosTeste.java
@@ -37,47 +37,55 @@ public class ArvoreGeradoraDeRotulosMinimosTeste {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         ArrayList<Integer> rotulosUtilizados = new ArrayList<>();
-        ArrayList<Grafo> grafos = LerArquivo.leArquivo(caminho2);
-        float funcObjetiva = 0;
-        long startTime = System.currentTimeMillis();
-        for(Grafo graph : grafos){
-            rotulosUtilizados = MVCA.MVCA(graph);
-            funcObjetiva += rotulosUtilizados.size();           
-        }
-        long endTime = System.currentTimeMillis();
-        System.out.println(funcObjetiva + " e " + grafos.size()); 
-        AGRMOutput((funcObjetiva/grafos.size()),"HD500", caminho2, (endTime - startTime));
+    	ArrayList<Grafo> grafos = LerArquivo.leArquivo(caminho2);
+    	
+    	JSONArray objPastas = new JSONArray();
+    	JSONObject objPasta = new JSONObject();
+    	
+    	float funcObjetiva = 0;
+    	long startTime = System.currentTimeMillis();
+    	
+    	//Fazer isso uma vez para cada pasta
+    	String pasta = "group2";
+    	objPasta.put("Diretorio", pasta);
+    	
+    	//Fazer o laço de grafos para cada arquivo dentro da pasta
+    	for(Grafo graph : grafos){
+    		rotulosUtilizados = MVCA.MVCA(graph);
+    		funcObjetiva += rotulosUtilizados.size();   		
+    	}
+    	long endTime = System.currentTimeMillis();
+		System.out.println(funcObjetiva + " e " + grafos.size());
+		
+		//fazer isso para cada Arquivo dentro da pasta X
+    	objPasta.put("Arquivos", AGRMOutput((funcObjetiva/grafos.size()), caminho2, (endTime - startTime)));
+    	
+    	//Apos gravar todos os arquivos da pasta X, gravar em Pastas e fazer o mesmo para a proxima pasta, até não existir mais pastas
+    	objPastas.add(objPasta);
+    	
+    	//Apos terminar todas as Pastas e arquivos, gravar Json de todos os resultados
+        try (FileWriter file = new FileWriter("Resultados.txt")) {    		
+            //Identar Output modo Pretty Print	
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();			
+            String teste = gson.toJson(objPastas);
 
+            //file.write(objRotas.toJSONString());
+            file.write(teste);    				
+        }
     }
     
-    public static void AGRMOutput (float rotulosUtilizados, String pasta, String arquivo, long time) throws IOException{
-        
-        
-        JSONArray objGrafos = new JSONArray();
-                      
-        JSONObject objGrafo = new JSONObject();
-        
-        objGrafo.put("Pasta", pasta);
-        objGrafo.put("Arquivo", arquivo);
-        objGrafo.put("Media de Rotulos Minimos", rotulosUtilizados);
-        objGrafo.put("Tempo de execucao", time + " ms");
-        
-        objGrafos.add(objGrafo);
-    
-    // try-with-resources statement based on post comment below :)
-        try (FileWriter file = new FileWriter("Resultados.txt")) {
-            
-//              String teste = objRotas.toString();
-            //Identar Output modo Pretty Print  
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();         
-            String teste = gson.toJson(objGrafos);
-            
-            //file.write(objRotas.toJSONString());
-            file.write(teste);
-            
-        }
+    public static JSONArray AGRMOutput (float rotulosUtilizados, String arquivo, long time) throws IOException{
+    	
+    	JSONArray objArquivos = new JSONArray();    	    	      
+		JSONObject objArquivo = new JSONObject();
+		
+		objArquivo.put("Arquivo", arquivo);
+		objArquivo.put("Media de Rotulos Minimos", rotulosUtilizados);
+		objArquivo.put("Tempo de execucao", time + " ms");
+		
+		objArquivos.add(objArquivo);
 
-         
-        }
+    return objArquivos;	 
+    }
 
 }
