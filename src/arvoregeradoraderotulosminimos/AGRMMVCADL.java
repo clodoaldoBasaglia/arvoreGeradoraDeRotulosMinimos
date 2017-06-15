f9b929bbdb8b3d34e80eb0971e3b8856a31eb522
Clodoaldo Basaglia <clodoaldofonseca92@gmail.com>
2017-06-15 01:56:44 -0300

    solucionando alguns problemas com o AGRMMVCADL

--- src/arvoregeradoraderotulosminimos/AGRMMVCADL.java
+++ src/arvoregeradoraderotulosminimos/AGRMMVCADL.java
@@ -35,29 +35,8 @@ public class AGRMMVCADL {
         JSONObject objPasta = new JSONObject();
 
         float funcObjetiva = 0;
-        long startTime = System.currentTimeMillis();
-        //Fazer isso uma vez para cada pasta
-        String pasta = "group2";
-        objPasta.put("Diretorio", pasta);
-        int qtdGrafos = 0;
 
-        //Fazer o laço de grafos para cada arquivo dentro da pasta
-        for (Grafo graph : grafos) {
-            //Se retornar nulo significa que o grafo não é conexo
-            if ((rotulosUtilizados = MVCA.MVCA(graph)) != null) {
-                funcObjetiva += rotulosUtilizados.size();
-                qtdGrafos++;
-            }
-        }
-        long endTime = System.currentTimeMillis();
 //        System.out.println(funcObjetiva + " e " + qtdGrafos);
-
-        //fazer isso para cada Arquivo dentro da pasta X
-        //Se a func objetivo for 0, quer dizer que todos os grafos do arquivo nao sao conexos.
-        if (funcObjetiva != 0) {
-            funcObjetiva = funcObjetiva / qtdGrafos;
-        }
-
         while (iterator.hasNext()) {
             String chave = iterator.next().getKey();
             ArrayList<String> get = sondaDeArquivos.get(chave);
@@ -71,6 +50,48 @@ public class AGRMMVCADL {
             }
         }
         System.out.println(mapaGrafos.size());
+        Iterator<Map.Entry<String, ArrayList<Grafo>>> iteratorMapaGrafos = mapaGrafos.entrySet().iterator();
+
+        int qtdGrafos = 0;
+        while (iteratorMapaGrafos.hasNext()) {
+            String key = iterator.next().getKey();
+            objPasta.put("Diretorio", key);
+            ArrayList<Grafo> get = mapaGrafos.get(key);
+            for (Grafo graph : get) {
+                //Se retornar nulo significa que o grafo não é conexo
+                if ((rotulosUtilizados = MVCA.MVCA(graph)) != null) {
+                    funcObjetiva += rotulosUtilizados.size();
+                    qtdGrafos++;
+                }
+            }
+            if (funcObjetiva != 0) {
+                funcObjetiva = funcObjetiva / qtdGrafos;
+            }
+
+        }
+        long startTime = System.currentTimeMillis();
+        String pasta = "group2";
+        //Fazer isso uma vez para cada pasta
+
+        //Fazer o laço de grafos para cada arquivo dentro da pasta
+        long endTime = System.currentTimeMillis();
+
+        //fazer isso para cada Arquivo dentro da pasta X
+        //Se a func objetivo for 0, quer dizer que todos os grafos do arquivo nao sao conexos.
+    }
+
+    public static JSONArray AGRMOutput(float rotulosUtilizados, String arquivo, int qtdConexos, long time) throws IOException {
+
+        JSONArray objArquivos = new JSONArray();
+        JSONObject objArquivo = new JSONObject();
+        objArquivo.put("Quantidade de grafos conexos", qtdConexos);
+        objArquivo.put("Arquivo", arquivo);
+        objArquivo.put("Media de Rotulos Minimos", rotulosUtilizados);
+        objArquivo.put("Tempo de execucao", time + " ms");
+
+        objArquivos.add(objArquivo);
+
+        return objArquivos;
     }
 
 }
